925505929e4b8fda7443fc0ab60cf30c2c340d4f
caiquesallasfatec <111195462+caiquesallasfatec@users.noreply.github.com>
2024-05-31 17:18:13 -0300

    Delete PwebnodeN directory

--- PwebnodeN/ExerciciosAnteriores/callback2.js
+++
@@ -1,8 +0,0 @@
-function exibeMensagemNaOrdem(mensagem, callback) {
-    console.log(mensagem)
-    callback()
-}
-
-exibeMensagemNaOrdem('essa é a primeira mensagem', function () {
-    console.log('essa é a segunda mensagem')
-})
--- PwebnodeN/ExerciciosAnteriores/quarto.js
+++
@@ -1,7 +0,0 @@
-var fs = require("fs")
-
-fileContent = fs.readFileSync('./file.txt')
-
-console.log(fileContent.toString())
-
-console.log('12345')
